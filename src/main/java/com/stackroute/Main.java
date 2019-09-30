0203de76835f696248b84d09dc1edf83b4914f3b
praveenawasthi01091 <praveenawasthi01091@gmail.com>
2019-09-30 15:37:28 +0530

    "Auto-wire xml Commit"

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -3,12 +3,14 @@ package com.stackroute;
 
 import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -16,11 +18,12 @@ import org.springframework.core.io.Resource;
 import java.beans.XMLDecoder;
 import java.io.FileNotFoundException;
 
+
 public class Main
 {
     public static void main( String[] args ) throws FileNotFoundException
     {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+       ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         /*without property*/
         Movie movie = (Movie) applicationContext.getBean("movie1");
         System.out.println(movie);
@@ -32,16 +35,6 @@ public class Main
         Movie movie2=(Movie)applicationContext.getBean("movie3");
         System.out.println(movie2);
 
-        /* Exception while using byType*/
-
-       /*Exception in thread "main" org.springframework.beans.factory.UnsatisfiedDependencyException:
-        Error creating bean with name 'movie3' defined in class path resource
-        [beans.xml]: Unsatisfied dependency expressed through bean property 'actor';
-        nested exception is org.springframework.beans.factory.NoUniqueBeanDefinitionException:
-        No qualifying bean of type 'com.stackroute.domain.Actor' available:
-        expected single matching bean but found 3: actor*/
-
     }
-
 
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,14 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements BeanNameAware, BeanFactoryAware, ApplicationContextAware {
+    private ApplicationContext applicationContext;
   private Actor actor;
 
     public Actor getActor() {
@@ -25,5 +33,26 @@ public class Movie {
         return "Movie{" +
                 "actor=" + actor +
                 '}';
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException
+    {
+        System.out.println("received the beanFactory: "+ beanFactory);
+
+    }
+
+    @Override
+    public void setBeanName(String s)
+    {
+        System.out.println("The name of  bean  is: "+s);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("applicationContext: "+ applicationContext);
+        this.applicationContext=applicationContext;
+
     }
 }
